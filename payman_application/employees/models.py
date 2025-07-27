0c98719b6136d2fcedb12af3ad6b88756cbcb9ba
zezor <kemma2993@gmail.com>
2025-07-27 15:11:42 -0800

    initial commit to payman

--- payman_application/employees/models.py
+++ payman_application/employees/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -9,7 +10,7 @@ class Employee(models.Model):
         ('other', 'other')
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     sur_name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
     other_name = models.CharField(max_length=200, blank=True, null=True)
--- payman_application/payman_application/settings.py
+++ payman_application/payman_application/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employees',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.user'
