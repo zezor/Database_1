e302227aaa2451378c63d7174dd630f731289337
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-07 16:22:36 +0000

    commit

--- payman_application/accounts/serializers.py
+++ payman_application/accounts/serializers.py
@@ -10,3 +10,55 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         # fields = '__all__'
         fields = ("email","password")
+
+
+# Create a custom representation for the serializer( Generic, Mixins, ViewSets)
+# for serializing HTTP request data
+class UserInputSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+# for deserializing HTTP response data
+class UserOutputSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ("email", "password")  
+
+
+class BookInputSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    author = serializers.CharField(max_length=100)
+    published_date = serializers.DateField()
+    is_recent = serializers.SerializerMethodField()
+    
+    #get_<field_name> method to compute the value of a SerializerMethodField
+    def get_is_recent(self, obj):
+        return "alx"
+    
+    #custom create and update methods
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.published_date = validated_data.get('published_date', instance.published_date)
+        instance.save()
+        return instance
+    
+    # FIELD-LEVEL VALIDATION
+        # validate_field_name method for field-level validation
+    def validate_published_date(self, value):
+        ...
+
+    def validadte_author(self, value):
+        ...
+
+    def validate_title(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long.")
+        return value
+
+ # OBJECT-LEVEL VALIDATION
+    def validate(self, data):
+        data
--- payman_application/employees/views.py
+++ payman_application/employees/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
